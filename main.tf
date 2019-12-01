d28bf8c2065fdb2f6a5456cfdf089f19ba0595c7
Kiran patil <kiranp295@gmail.com>
2019-12-01 10:30:58 +0000

    updated ver1.1

--- main.tf
+++ main.tf
@@ -1,21 +1,34 @@
+
+
 resource "google_compute_instance"  "default" {
-name = "test"
-machine_type = "n1-standard-1"
+count = "${length(var.name_count)}"
+name = "list-${count.index+1}"
+machine_type = var.machine_type["prod"]
 zone = "asia-south1-c"
 
 boot_disk {
     initialize_params {
-        image = "ubuntu-os-cloud/ubuntu-1604-lts"
+        image = var.image
     }
 }
 
 network_interface {
     network = "default"
-    network_ip = "10.160.0.5"
+    
     subnetwork = "default"
 }
 
 service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
 }
+}
+
+output "machine_type" {value = "${google_compute_instance.default.*.machine_type}"}
+
+output "name" {
+  value = "${google_compute_instance.default.*.name}"
+}
+
+output "zone" {
+  value = "${google_compute_instance.default.*.zone}"
 }
